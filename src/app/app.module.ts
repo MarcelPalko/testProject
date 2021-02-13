f0646f03e28083ba7769b2e5ff22291b1ec069cc
Marcel Palko <marcelpalko188@gmail.com>
2021-02-13 12:40:01 +0100

    Vytvoření základního layoutu + nastylování hlavičky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { UiSidenavComponent } from './ui-components/ui-sidenav/ui-sidenav.component';
+import { UiFooterComponent } from './ui-components/ui-footer/ui-footer.component';
+import { UiDashboardViewComponent } from './ui-components/ui-dashboard-view/ui-dashboard-view.component';
+import { UiHeaderComponent } from './ui-components/ui-header/ui-header.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: UiDashboardViewComponent },
+  { path: '',   redirectTo: '/dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiSidenavComponent,
+    UiFooterComponent,
+    UiDashboardViewComponent,
+    UiHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ui-components/ui-sidenav/ui-sidenav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-sidenav',
+  templateUrl: './ui-sidenav.component.html',
+  styleUrls: ['./ui-sidenav.component.scss']
+})
+export class UiSidenavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
